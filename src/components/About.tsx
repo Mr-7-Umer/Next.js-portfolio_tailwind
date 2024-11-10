388f90bad6644edb6714442c31cc7c8222cfcd79
Muhammad Umer <bsmumer@gmail.com>
2024-11-10 18:34:51 +0500

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="about" className='container pt-32'>
+  <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-up"> About Me</h2>
+  <p className='text-gray-500 pt-4' data-aos="zoom-in-up">
+  Hi, I’m Muhammad Umer, a beginner web developer with a growing skill set in HTML, CSS, Next.js, and Python.
+   I’m passionate about learning and excited to create dynamic, user-friendly websites as I build my skills. 
+   Thanks for visiting my portfolio—feel free to check out my projects!
+  </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Skills.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div id='skills' className='container pt-32'>
+
+        <div className='grid md:grid-cols-2 gap-20 items-center'>
+
+            <div data-aos="zoom-in-up">
+                <h2 className='text-4xl md:text-5xl text-gray-300'>Technologies I work On</h2>
+                <br />
+                <br />
+                <p className='text-gray-500 pt-2'>
+                I'm passionate about using the latest technologies 
+                to build modern,
+                scalable applications. 
+                Here are the technologies I work with.</p>
+            </div>
+            <div>
+                <div className='grid grid-cols-2 text-3xl sm:text-4xl'>
+                 
+                    <div className='space-y-2'>
+                        <ul>
+                        <li data-aos="zoom-in-up" className='text-blue-500'>Typescript</li>
+                        <li data-aos="zoom-in-up" className='text-white'>Next.js</li>
+                        <li data-aos="zoom-in-up" className='text-yellow-400'>Python</li>
+                        </ul>
+
+                    </div>
+                    
+                    <div className='space-y-2'>
+                        <ul>
+                        <li data-aos="zoom-in-up" className='text-teal-400'>Tailwind</li>
+                        <li data-aos="zoom-in-up" className='text-red-500'>Html</li>
+                        <li data-aos="zoom-in-up" className='text-accent'>Node.js</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
